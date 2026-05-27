80145d56a9a4457d9c5154d1982034dd19557fa7
nithil17 <nithilnayan@yahoo.com>
2026-05-27 12:22:12 +0530

    Clean up React entrypoint

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux';
 import { store } from './redux/store.js'
-import { useSelector } from 'react-redux'
-import { books } from './data/books.js'
 import './styles/app.css';
-const books = useSelector((state) => state.books.booksAll);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
